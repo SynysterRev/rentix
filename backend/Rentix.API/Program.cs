d9aecb580921527696934088bf1cf86700abefd2
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-10-28 12:12:10 +0100

    rename solution and add api versioning and database string connection

--- backend/Rentix.API/Program.cs
+++ backend/Rentix.API/Program.cs
@@ -1,9 +1,26 @@
+using Asp.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var apiVersioningBuilder = builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+});
+
+apiVersioningBuilder.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,9 +31,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
---
+++ backend/Rentix.API/StartupExtensions/ServicesExtension.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Rentix.API.StartupExtensions
+{
+    public static class ServicesExtension
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            if (environment.IsProduction())
+            {
+                var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                if (!string.IsNullOrEmpty(databaseUrl))
+                {
+                    var uri = new Uri(databaseUrl);
+                    var userInfo = uri.UserInfo.Split(':');
+
+                    var connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.LocalPath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+
+                    configuration["ConnectionStrings:Default"] = connectionString;
+                }
+            }
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //{
+            //    options.UseNpgsql(configuration.GetConnectionString("Default"));
+            //});
+
+            var originsString = configuration.GetValue<string>("AllowedOrigins");
+            var defaultOrigins = new string[] { "http://localhost:4200" };
+            var allowedOrigins = originsString?
+                            .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                            .Select(s => s.Trim())
+                            .ToArray()
+                            ?? defaultOrigins;
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                    .WithOrigins(allowedOrigins)
+                    .AllowAnyHeader()
+                    .WithMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .AllowCredentials();
+                });
+            });
+        }
+    }
+}
