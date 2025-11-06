8ecd82ba1ea81869180971d86f309a4dd6592569
JamyVED <78021641+JamyVED@users.noreply.github.com>
2025-11-06 17:01:39 +0100

    Update cards

--- frontend/src/app/domains/real-estate/components/property-card/property-card.ts
+++ frontend/src/app/domains/real-estate/components/property-card/property-card.ts
@@ -1,12 +1,20 @@
 import { Component, input } from '@angular/core';
 import { PropertyDTO } from '../../models/property.model';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, UsersRound } from "lucide-angular";
 
 @Component({
   selector: 'app-property-card',
-  imports: [],
+  imports: [MatCardModule, LucideAngularModule],
   templateUrl: './property-card.html',
   styleUrl: './property-card.scss',
 })
 export class PropertyCard {
+  readonly UsersRound = UsersRound;
   property = input<PropertyDTO>();
+
+  propertyIsAvailable(){
+    return this.property()?.isAvailable;
+  }
 }
